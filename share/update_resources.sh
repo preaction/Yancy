246369cfbc645843e95d18a93b159fda4aaa4a57
Doug Bell <doug@preaction.me>
2018-02-25 00:06:45 -0600

    upgrade to bootstrap 4.0.0 stable
    The stable release added back the offset classes, which we need for the
    new menu

--- share/update_resources.sh
+++ share/update_resources.sh
@@ -7,9 +7,9 @@ mkdir -p lib/Mojolicious/Plugin/Yancy/resources/public/yancy/font-awesome/{css,f
 
 curl https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js \
     > lib/Mojolicious/Plugin/Yancy/resources/public/yancy/jquery.js
-curl https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css \
+curl https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css \
     > lib/Mojolicious/Plugin/Yancy/resources/public/yancy/bootstrap.css
-curl https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js \
+curl https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js \
     > lib/Mojolicious/Plugin/Yancy/resources/public/yancy/bootstrap.js
 curl https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.8/umd/popper.min.js \
     > lib/Mojolicious/Plugin/Yancy/resources/public/yancy/popper.js
