b481cd49166f01ee7672a97ff4cbb8d69bee5714
Doug Bell <doug@preaction.me>
2018-10-20 15:49:10 -0500

    add edit form to test app
    This is how I'm testing the form generation to see how it works.

--- eg/test-app/yancy.pl
+++ eg/test-app/yancy.pl
@@ -3,6 +3,8 @@ use Mojolicious::Lite;
 
 plugin Config =>;
 plugin Yancy => app->config;
+app->yancy->plugin( 'Form::Bootstrap4' );
+app->defaults( layout => 'default' );
 
 app->routes->get( '/people' )->to(
     'yancy#list',
@@ -14,10 +16,28 @@ app->routes->get( '/people/:id' )->to(
     collection => 'people',
     template => 'view_people',
 )->name( 'people.get' );
+app->routes->any( [qw( GET POST )], '/people/:id/edit' )->to(
+    'yancy#set',
+    collection => 'people',
+    template => 'edit_people',
+    forward_to => 'people.get',
+)->name( 'people.edit' );
 
 app->start;
 
 __DATA__
+@@ layouts/default.html.ep
+<head>
+<link rel="stylesheet" href="/yancy/bootstrap.css">
+</head>
+<div class="container">
+<div class="row">
+<main class="col">
+%= content
+</main>
+</div>
+</div>
+
 @@ list_people.html.ep
 <ul>
 % for my $person ( @$items ) {
@@ -27,4 +47,10 @@ __DATA__
 
 @@ view_people.html.ep
 <h1><%= $item->{name} %></h1>
+%= link_to 'Edit', 'people.edit'
 %= link_to 'Back', 'people.list'
+
+@@ edit_people.html.ep
+<h1><%= $item->{name} %></h1>
+%= link_to 'Back', 'people.get'
+%= $c->yancy->form->form_for( 'people', method => 'POST', action => url_with() );
