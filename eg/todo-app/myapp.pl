4cb876847db127f54bf31e85e0ce0a4d7832ae6a
Doug Bell <doug@preaction.me>
2019-01-03 23:18:01 -0600

    make todo app work better on mobile devices

--- eg/todo-app/myapp.pl
+++ eg/todo-app/myapp.pl
@@ -212,15 +212,7 @@ __DATA__
 <div class="row">
     <div class="col-md-12">
 
-        <div class="d-flex justify-content-between align-items-center">
-            <a class="btn btn-outline-dark" href="<%= url_for 'todo.list' => ( date => $prev_date->ymd ) %>">
-                &lt; <%= $prev_date->ymd %>
-            </a>
-            <h1><%= $date->ymd %></h1>
-            <a class="btn btn-outline-dark" href="<%= url_for 'todo.list' => ( date => $next_date->ymd ) %>">
-                <%= $next_date->ymd %> &gt;
-            </a>
-        </div>
+        <h1 class="text-center"><%= $date->ymd %></h1>
 
         <ul class="list-group">
         % for my $item ( @$items ) {
@@ -244,6 +236,16 @@ __DATA__
         % }
         </ul>
 
+        <div class="d-flex flex-wrap mt-2 justify-content-between align-items-center">
+            <a class="btn btn-outline-dark" href="<%= url_for 'todo.list' => ( date => $prev_date->ymd ) %>">
+                &lt; <%= $prev_date->ymd %>
+            </a>
+            <a href="<%= url_for '/' %>" class="btn btn-secondary">Today</a>
+            <a class="btn btn-outline-dark" href="<%= url_for 'todo.list' => ( date => $next_date->ymd ) %>">
+                <%= $next_date->ymd %> &gt;
+            </a>
+        </div>
+
     </div>
 </div>
 
@@ -252,15 +254,15 @@ __DATA__
 <html>
     <head>
         <title><%= title %></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         %= stylesheet '/yancy/bootstrap.css'
     </head>
     <body>
         <main class="container">
             <%= content %>
         </main>
-        <footer class="container mt-2 d-flex justify-content-between">
-            <a href="<%= url_for '/' %>" class="btn btn-secondary">Today</a>
-            <a href="<%= url_for '/yancy' %>" class="btn btn-secondary">Edit</a>
+        <footer class="container mt-4 mb-2 d-flex justify-content-center">
+            <a href="<%= url_for '/yancy' %>">Edit Content</a>
         </footer>
     </body>
 </html>
