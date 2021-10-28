933033c06155f648af052d678128fa28cf9b9332
Doug Bell <doug@preaction.me>
2021-10-28 09:55:12 -0500

    fix example cpanfiles to for J::V upgrade

--- eg/doc-site/deploy.sh
+++ eg/doc-site/deploy.sh
@@ -5,5 +5,5 @@ deploy=$root/deploy
 if [ ! -d $deploy ]; then
     mkdir $deploy
 fi
-perl $root/myapp.pl export -m preaction --to $deploy
+carton exec -- perl $root/myapp.pl export -m preaction --to $deploy
 rsync -rvzm --delete $deploy/. preaction.me:/var/www/www.preaction.me/yancy/.
