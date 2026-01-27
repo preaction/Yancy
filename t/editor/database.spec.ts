4f11a4c164bf21e3c95275921dfa5b56591d92b4
Doug Bell <doug@preaction.me>
2026-01-27 16:12:25 -0600

    make a better example for database test
    This gives us a couple options for building the edit form.

--- t/editor/database.spec.ts
+++ t/editor/database.spec.ts
@@ -3,6 +3,18 @@ import EditorPage from "./EditorPage";
 
 test.describe("database editor", () => {
   const databaseSchema = {
+    situs: {
+      properties: {
+        situs_id: { type: "number", readOnly: true },
+        name: { type: "string" },
+        street_number: { type: "number", format: "integer" },
+        street_dir: { type: "string", enum: ["", "N", "E", "S", "W"] },
+        street_name: { type: "string" },
+        street_type: { type: "string", enum: ["ST", "AVE", "BLVD"] },
+        jurisdiction: { type: "string" },
+        legal_description: { type: "string", format: "textarea" },
+      },
+    },
     blocks: {
       title: "Blocks",
       properties: {
@@ -28,19 +40,35 @@ test.describe("database editor", () => {
   const databaseData: { [key: string]: Array<any> } = {
     blocks: [],
     pages: [],
+    situs: [],
   };
 
   test.beforeEach(async ({ page }) => {
-    await page.goto("/yancy");
     await page.route("*/**/api", async (route) => {
       const json = databaseSchema;
       await route.fulfill({ json });
     });
     await page.route("*/**/api/*", async (route, request) => {
-      const schemaName = request.url().split("/").slice(-1)[0];
-      const json = databaseData[schemaName];
-      await route.fulfill({ json: { items: json } });
+      const pathParts = request.url().split("/");
+      const [schemaName, ...idParts] = pathParts.slice(
+        pathParts.indexOf("api") + 1,
+      );
+      if (request.method() == "GET") {
+        const json = databaseData[schemaName];
+        if (idParts.length) {
+          // XXX: Get a single item
+        } else {
+          // TODO: Filters
+          // TODO: Pagination
+          await route.fulfill({ json: { items: json } });
+        }
+      } else if (request.method() == "POST" || request.method() == "PUT") {
+        // XXX: Create or update an item
+      } else if (request.method() == "DELETE") {
+        // XXX: Delete an item
+      }
     });
+    await page.goto("/yancy");
   });
 
   test.describe("database list", () => {
@@ -52,31 +80,49 @@ test.describe("database editor", () => {
         Object.keys(databaseSchema).length,
       );
       for (const [i, text] of Object.keys(databaseSchema).sort().entries()) {
-        await expect(databaseItems.nth(i)).toContainText(text);
+        await expect(databaseItems.nth(i)).toContainText(text, {
+          ignoreCase: true,
+        });
       }
     });
   });
 
   test.describe("shows data", () => {
     test("lists some data", async ({ page }) => {
-      databaseData["pages"] = [
+      databaseData["situs"] = [
         {
-          page_id: 1,
+          situs_id: 1,
           name: "index",
-          method: "GET",
-          pattern: "/",
-          title: "Home Page",
-          template: "index.html.ep",
-          in_app: true,
+          street_number: 123,
+          street_dir: "N",
+          street_name: "MAIN",
+          street_type: "ST",
+          jurisdiction: "City of Oshkosh",
         },
       ];
 
       const editor = new EditorPage(page);
-      const table = await editor.openDatabaseEditorForTable("pages");
+      const table = await editor.openDatabaseEditorForTable("situs");
 
-      await expect(table.locator("tbody tr")).toHaveCount(
-        databaseData["pages"].length,
-      );
+      const schemaColumns = Object.keys(databaseSchema["situs"].properties);
+      const columnHeadings = table.locator("thead th");
+      await expect(columnHeadings).toHaveCount(schemaColumns.length);
+      for (const [i, text] of schemaColumns.entries()) {
+        await expect(columnHeadings.nth(i)).toContainText(text);
+      }
+
+      const tableRows = table.locator("tbody tr");
+      await expect(tableRows).toHaveCount(databaseData["situs"].length);
+      for (const [i, dataRow] of databaseData["situs"].entries()) {
+        const tableFields = tableRows.nth(i).locator("td");
+        for (const [j, col] of schemaColumns.entries()) {
+          if (dataRow[col]) {
+            await expect(tableFields.nth(j)).toContainText(
+              dataRow[col].toString(),
+            );
+          }
+        }
+      }
     });
   });
 });
