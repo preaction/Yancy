312db68349b6cc7ac47587e6e0f5499b67efb0e6
Doug Bell <doug@preaction.me>
2026-01-29 17:54:55 -0600

    fix playwright not starting mojolicious app
    Was it localhost couldn't resolve? Or was it resolving to IPv6? What?

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "@playwright/test";
 
-const baseURL = process.env.YANCY_EDITOR_URL || "http://localhost:3333";
+const baseURL = process.env.YANCY_EDITOR_URL || "http://127.0.0.1:3333";
 export default defineConfig({
   webServer: {
     name: "mojo",
     command: `perl -Ilib myapp.pl daemon -l ${baseURL}`,
     reuseExistingServer: true,
-    url: baseURL + "/",
+    url: baseURL,
     stdout: "pipe",
     stderr: "pipe",
   },
