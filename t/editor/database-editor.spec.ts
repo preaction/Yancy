d301bcafc49c5143bbebbb9f7b9efa6c0fd05927
Doug Bell <doug@preaction.me>
2026-02-04 17:47:16 -0600

    refactor test mock API a bit

--- t/editor/database-editor.spec.ts
+++ t/editor/database-editor.spec.ts
@@ -31,29 +31,88 @@ const schema = {
     },
   },
 };
-const items = [
-  { schema_id: 1, name: "One" },
-  { schema_id: 2, name: "Two" },
-];
+const data: { [key: string]: any[] } = {
+  schema: [
+    { schema_id: 1, name: "One" },
+    { schema_id: 2, name: "Two" },
+  ],
+};
 
 const handlers = [
   http.get("./api", () => {
     return HttpResponse.json({ schema });
   }),
-  http.get("./api/schema", () => {
-    return HttpResponse.json({ items });
-  }),
-  http.post("./api/schema/", () => {
-    // XXX: Make this actually edit the items
-    items.push({ schema_id: 3, name: "Three" });
-    return HttpResponse.json({ schema_id: 3 });
-  }),
-  http.post("./api/schema/1", () => {
-    // XXX: Make this actually edit the items
-    items[0].name = "One and more";
-    return HttpResponse.arrayBuffer(new ArrayBuffer(), { status: 204 });
-  }),
+  http.get<{ id?: string; schema: string }>(
+    "./api/:schema/:id?",
+    async ({ params }) => {
+      if (!params.id) {
+        // Fetching a list
+        // TODO: Pagination from query params
+        return HttpResponse.json({ items: data[params.schema] });
+      }
+      // Fetching a single item
+      // TODO: Support x-id-field completely
+      const item = data[params.schema].find(
+        (item) => item.schema_id.toString() === params.id.toString(),
+      );
+      if (!item) {
+        return HttpResponse.json({ error: "Not found" }, { status: 404 });
+      }
+      return HttpResponse.json(item);
+    },
+  ),
+  http.post<{ id?: string; schema: string }>(
+    "./api/:schema/:id?",
+    async ({ request, params }) => {
+      const postItem = (await request.clone().json()) as any;
+      // TODO: Must validate the item against schema use "ajv" npm module
+
+      if (!postItem) {
+        return HttpResponse.json({ error: "No body" }, { status: 400 });
+      }
+
+      if (params.id) {
+        // We're updating an existing item
+        // TODO: Support x-id-field completely
+        const idx = data[params.schema].findIndex(
+          (item) => item.schema_id.toString() === params.id?.toString(),
+        );
+        if (idx < 0) {
+          return HttpResponse.json({ error: "Not found" }, { status: 404 });
+        }
+        data[params.schema][idx] = Object.assign(
+          data[params.schema][idx],
+          postItem,
+        );
+        return HttpResponse.json(data[params.schema][idx], { status: 204 });
+      }
+      // We're adding a new item
+      // TODO: Generate new autoinc ID
+      const newItemId = data[params.schema].length;
+      postItem.schema_id = newItemId;
+      data[params.schema].push(postItem);
+      return HttpResponse.arrayBuffer(new ArrayBuffer(newItemId), {
+        status: 201,
+      });
+    },
+  ),
+  http.delete<{ id: string; schema: string }>(
+    "./api/:schema/:id",
+    async ({ params }) => {
+      const idx = data[params.schema].findIndex(
+        (item) => item.schema_id.toString() === params.id?.toString(),
+      );
+      if (idx < 0) {
+        return HttpResponse.json({ error: "Not found" }, { status: 404 });
+      }
+      data[params.schema].splice(idx, 1);
+      return HttpResponse.arrayBuffer(new ArrayBuffer(), { status: 204 });
+    },
+  ),
 ];
+
+// XXX: We also need Yancy::Storage mock handlers
+
 const server = setupServer(...handlers);
 beforeAll(() => {
   server.listen({ onUnhandledRequest: "error" });
