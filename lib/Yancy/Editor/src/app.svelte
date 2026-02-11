f443927c3c25a75b4bce84d95840040b5f3180fc
Doug Bell <doug@preaction.me>
2026-02-11 16:18:35 -0600

    use pico in v2 editor
    This is not finished, but will work until I get the SPA internal app
    linking working. Then I'll be able to use links instead of buttons for
    internal linking.

--- lib/Yancy/Editor/src/app.svelte
+++ lib/Yancy/Editor/src/app.svelte
@@ -9,41 +9,43 @@
 </script>
 
 <div class="yancy-editor">
-  <nav class="yancy-accordion">
-    <h2 id="content-tree-button">
-      <button
-        onclick={() => (currentTab = "content")}
-        aria-controls="content-tree"
-        aria-expanded={currentTab == "content"}>Content</button
+  <aside>
+    <nav class="yancy-accordion">
+      <h2 id="content-tree-button">
+        <button
+          onclick={() => (currentTab = "content")}
+          aria-controls="content-tree"
+          aria-expanded={currentTab == "content"}>Content</button
+        >
+      </h2>
+      <section
+        id="content-tree"
+        class="accordion-panel"
+        aria-labelledby="content-tree-button"
       >
-    </h2>
-    <section
-      id="content-tree"
-      class="accordion-panel"
-      aria-labelledby="content-tree-button"
-    >
-      <ContentTree onselect={() => (currentTab = "content")}></ContentTree>
-    </section>
-    <h2 id="database-button">
-      <button
-        onclick={() => (currentTab = "database")}
-        aria-controls="database-list"
-        aria-expanded={currentTab == "database"}>Database</button
+        <ContentTree onselect={() => (currentTab = "content")}></ContentTree>
+      </section>
+      <h2 id="database-button">
+        <button
+          onclick={() => (currentTab = "database")}
+          aria-controls="database-list"
+          aria-expanded={currentTab == "database"}>Database</button
+        >
+      </h2>
+      <section
+        id="database-list"
+        class="accordion-panel"
+        aria-labelledby="database-button"
       >
-    </h2>
-    <section
-      id="database-list"
-      class="accordion-panel"
-      aria-labelledby="database-button"
-    >
-      <DatabaseList
-        onselect={(schema: string) => {
-          currentTab = "database";
-          currentSchema = schema;
-        }}
-      ></DatabaseList>
-    </section>
-  </nav>
+        <DatabaseList
+          onselect={(schema: string) => {
+            currentTab = "database";
+            currentSchema = schema;
+          }}
+        ></DatabaseList>
+      </section>
+    </nav>
+  </aside>
   <main>
     <div class="main-container">
       {#if currentTab == "content"}
@@ -71,8 +73,9 @@
     height: 100%;
     overflow: hidden;
   }
-  .yancy-editor > nav {
+  .yancy-editor > aside {
     flex: 0 1 20%;
+    overflow-y: scroll;
   }
   .yancy-editor > main {
     flex: 0 1 100%;
--- lib/Yancy/Editor/src/content-tree.svelte
+++ lib/Yancy/Editor/src/content-tree.svelte
@@ -55,9 +55,8 @@
     margin: 0;
     padding: 0.2em 0.8em;
     display: flex;
+    width: 100%;
     flex-flow: row wrap;
     justify-content: space-between;
-    border: none;
-    background: none;
   }
 </style>
