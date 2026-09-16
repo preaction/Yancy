58e435e260768f8fe1379498d770782b935a7aa4
Doug Bell <doug@preaction.me>
2026-09-15 20:35:30 -0500

    add some more content editor tools

--- lib/Yancy/Editor/src/content-field.svelte
+++ lib/Yancy/Editor/src/content-field.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
-  import { onMount, onDestroy, tick, untrack } from "svelte";
-  import { Menu, Portal, ToggleGroup } from "@skeletonlabs/skeleton-svelte";
+  import { onMount, onDestroy, tick } from "svelte";
+  import {
+    Menu,
+    Popover,
+    Portal,
+    usePopover,
+  } from "@skeletonlabs/skeleton-svelte";
+  import type { OpenChangeDetails } from "@zag-js/popover";
+  import { Color } from "@tiptap/extension-text-style";
+  import { ListItem } from "@tiptap/extension-list";
+  import { TextStyle } from "@tiptap/extension-text-style";
+  import { TextStyleKit } from "@tiptap/extension-text-style";
+  import Emoji from "@tiptap/extension-emoji";
+
   import { Editor } from "@tiptap/core";
   import { StarterKit } from "@tiptap/starter-kit";
-  import { shift } from "@floating-ui/dom";
   import {
     BoldIcon,
     CheckIcon,
     ItalicIcon,
+    LinkIcon,
+    ListIcon,
+    ListIndentDecreaseIcon,
+    ListIndentIncreaseIcon,
+    ListOrderedIcon,
     PlusIcon,
+    RedoIcon,
     UnderlineIcon,
+    UndoIcon,
   } from "@lucide/svelte";
   import type { ClassValue, SvelteHTMLElements } from "svelte/elements";
 
@@ -60,7 +78,27 @@
       editorProps: {
         attributes: { id, name, "aria-label": label },
       },
-      extensions: [StarterKit],
+      extensions: [
+        TextStyleKit.configure({
+          color: {
+            types: [TextStyle.name, ListItem.name],
+          },
+          textStyle: {
+            types: [ListItem.name],
+          },
+        }),
+        Emoji.configure({
+          enableEmoticons: true,
+        }),
+        StarterKit.configure({
+          link: {
+            openOnClick: false,
+            HTMLAttributes: {
+              rel: null,
+            },
+          },
+        }),
+      ],
       content: value,
       onTransaction: ({ editor }) => {
         // Update the state signal to force a re-render
@@ -127,9 +165,36 @@
         });
       },
     },
+    {
+      value: "blockquote",
+      label: "Block quote",
+      apply() {
+        editorState.editor?.chain().focus().toggleBlockquote().run();
+      },
+      isActive() {
+        return editorState.editor?.isActive("blockquote");
+      },
+    },
+    {
+      value: "pre",
+      label: "Preformatted",
+      apply() {
+        editorState.editor?.chain().focus().toggleCodeBlock().run();
+      },
+      isActive() {
+        return editorState.editor?.isActive("codeBlock");
+      },
+    },
   ];
 
   const insertNodes = [
+    {
+      value: "hr",
+      label: "Horizontal Rule",
+      apply() {
+        editorState.editor?.chain().focus().setHorizontalRule().run();
+      },
+    },
     {
       value: "img",
       label: "Image",
@@ -139,10 +204,27 @@
     },
   ];
 
+  const uid = $props.id();
+  const linkPopover = usePopover({
+    id: uid,
+    onOpenChange(details) {
+      showLink = details.open;
+      if (!details.open) {
+        if (!linkHref) {
+          editorState.editor?.chain().focus().unsetLink().run();
+        } else {
+          editorState.editor?.chain().focus().setLink({ href: linkHref }).run();
+        }
+      }
+    },
+  });
+  let showLink = $state(false);
+  let linkHref = $derived(editorState.editor?.getAttributes("link").href);
+
   let menuOpen = $state(false);
   let onToolbar = $state(false);
   let showToolbar = $derived(
-    editorState.editor?.isFocused || menuOpen || onToolbar,
+    editorState.editor?.isFocused || menuOpen || onToolbar || showLink,
   );
 </script>
 
@@ -170,7 +252,7 @@
           {#snippet element(attributes)}
             <button
               aria-label="Insert"
-              class="btn btn-sm preset-filled-primary-500"
+              class="btn-icon btn-icon-base preset-filled-primary-500"
               {...attributes}><PlusIcon /></button
             >
           {/snippet}
@@ -179,7 +261,7 @@
           <Menu.Positioner>
             <Menu.Content data-theme="cerberus">
               {#each insertNodes as item (item)}
-                <Menu.Item value={item.value}>
+                <Menu.Item value={item.value} onclick={() => item.apply()}>
                   <Menu.ItemText>{item.label}</Menu.ItemText>
                 </Menu.Item>
               {/each}
@@ -187,6 +269,25 @@
           </Menu.Positioner>
         </Portal>
       </Menu>
+
+      <span class="vr"></span>
+
+      <button
+        type="button"
+        aria-label="Undo"
+        onclick={() => editorState.editor?.chain().focus().undo().run()}
+        class="btn-icon btn-icon-base preset-tonal"
+        disabled={!editorState.editor?.can().chain().focus().undo().run()}
+        ><UndoIcon class="size-4" /></button
+      >
+      <button
+        type="button"
+        aria-label="Redo"
+        onclick={() => editorState.editor?.chain().focus().redo().run()}
+        class="btn-icon btn-icon-base preset-tonal"
+        disabled={!editorState.editor?.can().chain().focus().redo().run()}
+        ><RedoIcon class="size-4" /></button
+      >
 
       <span class="vr"></span>
 
@@ -200,10 +301,10 @@
           {#snippet element(attributes)}
             <button
               aria-label="Text style"
-              class="block btn btn-sm preset-outlined-primary-500 w-40 text-left"
+              class="block btn btn-base preset-tonal w-40 text-left"
               {...attributes}
               >{textStyles.find((n) => n.isActive())?.label ??
-                "Body text"}</button
+                "Body Text"}</button
             >
           {/snippet}
         </Menu.Trigger>
@@ -234,7 +335,7 @@
         type="button"
         aria-label="Bold"
         onclick={() => editorState.editor?.chain().focus().toggleBold().run()}
-        class="btn {editorState.editor?.isActive('bold')
+        class="btn-icon btn-icon-base {editorState.editor?.isActive('bold')
           ? 'preset-filled'
           : 'preset-tonal'}"><BoldIcon class="size-4" /></button
       >
@@ -242,7 +343,7 @@
         type="button"
         aria-label="Italic"
         onclick={() => editorState.editor?.chain().focus().toggleItalic().run()}
-        class="btn {editorState.editor?.isActive('italic')
+        class="btn-icon btn-icon-base {editorState.editor?.isActive('italic')
           ? 'preset-filled'
           : 'preset-tonal'}"><ItalicIcon class="size-4" /></button
       >
@@ -251,9 +352,94 @@
         aria-label="Underline"
         onclick={() =>
           editorState.editor?.chain().focus().toggleUnderline().run()}
-        class="btn {editorState.editor?.isActive('underline')
+        class="btn-icon btn-icon-base {editorState.editor?.isActive('underline')
           ? 'preset-filled'
           : 'preset-tonal'}"><UnderlineIcon class="size-4" /></button
+      >
+      <Popover.Provider value={linkPopover}>
+        <Popover.Trigger class="btn-icon btn-icon-base preset-tonal"
+          ><LinkIcon /></Popover.Trigger
+        >
+        <Portal>
+          <Popover.Positioner data-theme="cerberus">
+            <Popover.Content
+              class="card max-w-md p-4 bg-surface-100-900 shadow-xl"
+            >
+              <Popover.Description>
+                <input
+                  bind:value={linkHref}
+                  class="input"
+                  onkeydown={(ev: KeyboardEvent) => {
+                    if (ev.key === "Enter") linkPopover().setOpen(false);
+                  }}
+                />
+                <button
+                  type="button"
+                  onclick={() => {
+                    linkPopover().setOpen(false);
+                  }}
+                  class="btn preset-filled-primary-500">Apply</button
+                >
+                <button
+                  type="button"
+                  class="btn preset-tonal"
+                  onclick={() => {
+                    linkHref = "";
+                    linkPopover().setOpen(false);
+                  }}>Remove</button
+                >
+              </Popover.Description>
+              <Popover.Arrow
+                class="[--arrow-size:--spacing(2)] [--arrow-background:var(--color-surface-100-900)]"
+              >
+                <Popover.ArrowTip />
+              </Popover.Arrow>
+            </Popover.Content>
+          </Popover.Positioner>
+        </Portal>
+      </Popover.Provider>
+      <span class="vr"></span>
+
+      <button
+        type="button"
+        aria-label="Bulleted list"
+        onclick={() =>
+          editorState.editor?.chain().focus().toggleBulletList().run()}
+        class="btn-icon btn-icon-base {editorState.editor?.isActive(
+          'bulletList',
+        )
+          ? 'preset-filled'
+          : 'preset-tonal'}"><ListIcon class="size-4" /></button
+      >
+      <button
+        type="button"
+        aria-label="Ordered list"
+        onclick={() =>
+          editorState.editor?.chain().focus().toggleOrderedList().run()}
+        class="btn-icon btn-icon-base {editorState.editor?.isActive(
+          'orderedList',
+        )
+          ? 'preset-filled'
+          : 'preset-tonal'}"><ListOrderedIcon class="size-4" /></button
+      >
+
+      <button
+        type="button"
+        aria-label="Decrease indent"
+        onclick={() =>
+          editorState.editor?.chain().focus().liftListItem("listItem").run()}
+        class="btn-icon btn-icon-base preset-tonal"
+        disabled={!editorState.editor?.can().liftListItem("listItem")}
+        ><ListIndentDecreaseIcon class="size-4" /></button
+      >
+      <button
+        type="button"
+        aria-label="Increase indent"
+        onclick={() =>
+          editorState.editor?.chain().focus().sinkListItem("listItem").run()}
+        class="btn-icon btn-icon-base preset-tonal"
+        disabled={!editorState.editor?.can().sinkListItem("listItem")}
+        ><ListIndentIncreaseIcon class="size-4" /></button
       >
     </div>
   </Portal>
